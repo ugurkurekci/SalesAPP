894dd893dc792810bcc6f76446411c2032491fc9
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-02 23:52:02 +0300

    UnitOfWork design pattern repository added

--- Sales.Api.Domain/Repository/IUnitOfWorkRepository.cs
+++ Sales.Api.Domain/Repository/IUnitOfWorkRepository.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sales.Api.Domain.Repository.Abstract;
 
 namespace Sales.Api.Domain.Repository;
-internal interface IUnitOfWorkRepository
+
+public interface IUnitOfWorkRepository : IDisposable
 {
+
+    public IUserRepository UserRepository { get; }
+
+    public int Complete();
+
 }
--- Sales.Api.Domain/Repository/UnitOfWorkRepository.cs
+++ Sales.Api.Domain/Repository/UnitOfWorkRepository.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sales.Api.Domain.DataAccess;
+using Sales.Api.Domain.Repository.Abstract;
 
 namespace Sales.Api.Domain.Repository;
-public class UnitOfWorkRepository
+
+public class UnitOfWorkRepository : IUnitOfWorkRepository
 {
+
+    public readonly SalesDbContext _salesDbContext;
+
+    public UnitOfWorkRepository(SalesDbContext salesDbContext, IUserRepository userRepository)
+    {
+        _salesDbContext = salesDbContext;
+        UserRepository = userRepository;
+    }
+
+    public IUserRepository UserRepository { get; }
+
+    public int Complete()
+    {
+        return _salesDbContext.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _salesDbContext.Dispose();
+    }
 }
