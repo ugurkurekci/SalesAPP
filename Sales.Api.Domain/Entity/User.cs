fc9f19f8fa6858701f28f46b68564f10b71a52d8
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-02 23:50:12 +0300

    User model added

--- Sales.Api.Domain/Entity/User.cs
+++ Sales.Api.Domain/Entity/User.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Sales.Api.Domain.Entity;
 
-namespace Sales.Api.Domain.Entity;
-public class User
+public class User : EntityBase
 {
+
+    public string Username { get; set; }
+
+    public string EMail { get; set; }
+
+    public string Password { get; set; }
+
 }
