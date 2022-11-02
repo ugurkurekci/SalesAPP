adef3f247087d4e304e1fb568ca847bd7383a221
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-02 23:50:35 +0300

    User register Dtos added

---
+++ Sales.Api.Business/Dto/UserRegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Api.Business.Dto;
+
+public class UserRegisterDto
+{
+
+    public string Email { get; set; }
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+}
