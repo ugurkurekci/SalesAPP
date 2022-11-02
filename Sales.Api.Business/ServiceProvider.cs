f52d3eeecc9d6a28811118fe51561f3573bffe6b
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-02 23:51:36 +0300

    services collection added

--- Sales.Api.Business/ServiceProvider.cs
+++ Sales.Api.Business/ServiceProvider.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Sales.Api.Business.Services.Abstract;
+using Sales.Api.Business.Services.Concrete;
+using Sales.Api.Domain.DataAccess;
+using Sales.Api.Domain.Repository;
+using Sales.Api.Domain.Repository.Abstract;
+using Sales.Api.Domain.Repository.Concrete;
+using System.Reflection;
 
 namespace Sales.Api.Business;
-public class ServiceProvider
+
+public static class ServiceProvider
 {
+    public static void MyDependencyInjections(this IServiceCollection services)
+    {
+
+        // DbContext
+        services.AddTransient<SalesDbContext>();
+
+
+        // AutoMapper
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        //AutoMapperExtensions
+
+        //AutoMappers
+        services.AddMediatR(typeof(IAssemblyMarker).Assembly);
+
+        // Repositories
+
+        services.AddScoped<IUnitOfWorkRepository, UnitOfWorkRepository>();
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        // Services
+
+        services.AddScoped<IUserService, UserService>();
+
+
+    }
+
 }
