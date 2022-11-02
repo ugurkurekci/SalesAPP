5e2f12f1c3e34a5f367d90251caf0bcb68f73e71
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-02 23:52:34 +0300

    EntityBase added

---
+++ Sales.Api.Domain/Entity/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Api.Domain.Entity;
+
+public class EntityBase
+{
+
+    [Key]
+    public int ID { get; set; }
+
+    public DateTime CreateDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+}
