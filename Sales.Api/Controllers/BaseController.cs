c0c38d677fb3d9915417cc3055d09725006646df
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-03 21:00:37 +0300

    User endpoint added

---
+++ Sales.Api/Controllers/BaseController.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sales.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase { }
---
+++ Sales.Api/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.Api.Business.Dto;
+using Sales.Api.Business.Services.Abstract;
+
+namespace Sales.Api.Controllers;
+
+public class UserController : BaseController
+{
+
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+    {
+
+        Result result = await _userService.GetUserLogin(userLoginDto);
+        if (result.Success == true)
+        {
+            return Ok(result);
+        }
+
+        return BadRequest(result.Message);
+
+    }
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+    {
+
+        Result result = await _userService.GetUserRegister(userRegisterDto);
+        if (result.Success == true)
+        {
+            return Ok(result);
+        }
+
+        return BadRequest(result.Message);
+
+    }
+
+}
