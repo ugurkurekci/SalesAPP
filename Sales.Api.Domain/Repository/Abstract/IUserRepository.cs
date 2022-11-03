4041d75edd22417641a7d9a52289cba938c8554f
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-03 21:01:57 +0300

    user repository added

--- Sales.Api.Domain/Repository/Abstract/IUserRepository.cs
+++ Sales.Api.Domain/Repository/Abstract/IUserRepository.cs
@@ -1,8 +1,20 @@
 ﻿using Sales.Api.Domain.Entity;
-using Sales.Api.Domain.Repository.GenericBaseRepository;
+using System.Linq.Expressions;
 
 namespace Sales.Api.Domain.Repository.Abstract;
 
-public interface IUserRepository : IRepositoryBase<User>
+public interface IUserRepository
 {
+    Task<IReadOnlyList<User>> GetAll();
+
+    Task<IReadOnlyList<User>> GetAllByFilter(Expression<Func<User, bool>> filter);
+
+    Task<User> GetByFilter(Expression<Func<User, bool>> filter);
+
+    Task Add(User entity);
+
+    Task Update(User entity);
+
+    Task Delete(User entity);
+
 }
--- Sales.Api.Domain/Repository/Concrete/UserRepository.cs
+++ Sales.Api.Domain/Repository/Concrete/UserRepository.cs
@@ -1,14 +1,61 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using Sales.Api.Domain.DataAccess;
 using Sales.Api.Domain.Entity;
 using Sales.Api.Domain.Repository.Abstract;
-using Sales.Api.Domain.Repository.GenericBaseRepository;
+using System.Linq.Expressions;
 
 namespace Sales.Api.Domain.Repository.Concrete;
 
-public class UserRepository : RepositoryBase<User>, IUserRepository
+public class UserRepository : IUserRepository
 {
-    public UserRepository(DbContext context, IMapper mapper) : base(context, mapper)
+
+    private readonly SalesDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UserRepository(SalesDbContext context, IMapper mapper)
     {
+        _context = context;
+        _mapper = mapper;
     }
+
+    public async Task<IReadOnlyList<User>> GetAll()
+    {
+        return await _context.Set<User>().ProjectTo<User>(_mapper.ConfigurationProvider).ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<User>> GetAllByFilter(Expression<Func<User, bool>> filter)
+    {
+        return await _context.Set<User>().ProjectTo<User>(_mapper.ConfigurationProvider).Where(filter).ToListAsync();
+    }
+
+    public async Task<User> GetByFilter(Expression<Func<User, bool>> filter)
+    {
+        return await _context.Set<User>().ProjectTo<User>(_mapper.ConfigurationProvider).FirstOrDefaultAsync(filter);
+    }
+
+    public Task Add(User entity)
+    {
+        _context.User.Add(entity);
+        _context.SaveChanges();
+        return Task.CompletedTask;
+
+    }
+
+    public Task Delete(User entity)
+    {
+        entity.IsDeleted = true;
+        _context.User.Update(entity);
+        _context.SaveChanges();
+        return Task.CompletedTask;
+    }
+
+    public Task Update(User entity)
+    {
+        _context.User.Update(entity);
+        _context.SaveChanges();
+        return Task.CompletedTask;
+    }
+
 }
