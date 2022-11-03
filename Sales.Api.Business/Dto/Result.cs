be04d3f6a8635a01197eed5c7ab8dc8fb5dc30f8
UGUR <UGUR@DESKTOP-33C9DHR>
2022-11-03 21:00:48 +0300

    result added

---
+++ Sales.Api.Business/Dto/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Api.Business.Dto;
+
+public class Result
+{
+
+    public string Message { get; set; }
+
+    public bool Success { get; set; }
+
+    public object Data { get; set; }
+
+}
